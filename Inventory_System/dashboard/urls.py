55db3fcd0acd6ded1f375118cf98a428644a0591
Adrian Andrin <adrianandrin13@gmail.com>
2020-09-11 04:59:20 +0800

    Arrange Files

---
+++ Inventory_System/dashboard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='dashboard-index'),
+    path('dashboard/', views.home, name='dashboard-home'),
+    path('dashboard/customer/', views.CustomerView.as_view(),
+         name='dashboard-customer'),
+    path('dashboard/register/', views.register, name='dashboard-register'),
+    path('dashboard/register2/', views.register2, name='dashboard-register2'),
+]
---
+++ Inventory_System/dashboard/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+
+def index(request):
+    return render(request, 'dashboard/index.html')
+
+
+def home(request):
+    return render(request, 'dashboard/home.html')
+
+
+class CustomerView(View):
+    def get(self, request):
+        return render(request, 'dashboard/customer.html')
+
+    def post(self, request):
+        return render(request, 'dashboard/register.html')
+
+
+def register(request):
+    return render(request, 'dashboard/register.html')
+
+
+def register2(request):
+    return render(request, 'dashboard/registration2.html')
